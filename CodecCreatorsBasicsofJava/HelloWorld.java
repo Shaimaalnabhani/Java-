874707b1f978356a8bb3aee42ccf49ee5920433f
Shaima <shaimaalnabhani@gmail.com>
2025-06-26 14:21:05 +0400

    Initial commit of Java project

---
+++ CodecCreatorsBasicsofJava/HelloWorld.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+public class   HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("I am Shaima AL-Nabhanai. ");
+        System.out.println("I am from Nizwa and I born in 24 of November 1999. ");
+        System.out.println("I graduated from university of technology and applied science as a telecommunication and electronics Engineer. ");
+        System.out.println("I tranied at Ooredoo NOC in 2023 , and assign to be a VAS Engineer.");
+        System.out.println("Also, I tranied at Ministry of Information in 2022 , and assign to be a Telecommunication Engineer.");
+        System.out.println("In 2022 ,I made a Smart follower Trolly bag as the graduation project.");
+        System.out.println("The goal of the project is to make the bag follow the owner and reduce the problem of bag theft due to security.");
+        System.out.println("It is done by using Arduino Mega, bluetooth module, ultrasonic sensor,LCD display,motor drive , LEDs and buzzer.");
+        System.out.println("The sensor will detect the distance between the bag and the owner , based on the distance , it moves to follow the owner.");
+        System.out.println("I am excellent interpersonal skill and excellent knowledge of computing application .");
+        System.out.println("I am excellent command of both written and spoken English and Arabic languges and good communication appreciation for the different communication style required when working with others .");
+        System.out.println("I have a HCIA-5G certificate provided by Huawei in 2023 .");
+        System.out.println("I have an Introduction to data science certificate provided by CISCO Network Academy in 2024 .");
+        System.out.print("I have an Introduction to data science certificate provided by CISCO Network Academy in 2024 .");
+    }
+    
+}
