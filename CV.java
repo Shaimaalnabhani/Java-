077933430c219bc00dec265abac58d4cf26bd12b
Shaima <shaimaalnabhani@gmail.com>
2025-06-30 11:25:06 +0400

    printing CV on cmd

---
+++ CV.java
@@ -0,0 +1,20 @@
+public class CV 
+	{
+		public static void main (String[] args)
+			{
+				System.out.println("I am Shaima AL-Nabhanai. ");
+				System.out.println("I am from Nizwa and I born in 24 of November 1999. ");
+				System.out.println("I have a bachelor degree Telecommunication and electronics Engineer with GPA 3.45 from university of technology and applied science in 2022. ");
+				System.out.println("I tranied at Ooredoo NOC in 2023, from April till October , and assign to be a VAS Engineer.");
+				System.out.println("Also, I tranied at Ministry of Information in 2022 for two months  , and assign to be a Telecommunication Engineer.");
+				System.out.println("In 2022 ,I made a Smart follower Trolly bag as the graduation project.");
+				System.out.println("The goal of the project is to make the bag follow the owner and reduce the problem of bag theft due to security.");
+				System.out.println("It is done by using Arduino Mega, bluetooth module, ultrasonic sensor,LCD display,motor drive , LEDs and buzzer.");
+				System.out.println("The sensor will detect the distance between the bag and the owner , based on the distance , it moves to follow the owner.");
+				System.out.println("I am excellent interpersonal skill and excellent knowledge of computing application .");
+				System.out.println("I am excellent command of both written and spoken English and Arabic languges and good communication appreciation for the different communication style required when working with others .");
+				System.out.println("I have a HCIA-5G certificate provided by Huawei in 2023 .");
+				System.out.println("I have an Introduction to data science certificate provided by CISCO Network Academy in 2024 .");
+			
+			}
+	}
